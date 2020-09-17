7e12fec97f80921c730b1644732ba9375e1ad9f5
Brandon <bredmond1019@gmail.com>
2020-09-17 12:35:10 -0400

    Updated flaskpractice

--- flask_practice.py
+++ flask_practice.py
@@ -44,7 +44,7 @@ def index():
 
 '''
 To run the app, you need to type to the command line:
-    FLASK_APP=<name_of_file> flask run
+    FLASK_APP=<name_of_file> FLASK_Debug=true flask run
 '''
 
 
@@ -54,3 +54,35 @@ person = Person(name='Felipe')
 db.session.add(person)
 db.session.commit()
 '''
+
+
+
+
+
+
+
+
+
+
+
+"""
+Example: A driver has many vehicles
+"""
+
+
+class Driver(db.Model):
+    __tablename__ = 'drivers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable = False)
+    state = db.Column(db.String(2), nullable = False)
+    issued = db.Column(db.DateTime())
+    vehicle = db.relationship("Vehicle", backref = 'drivers', lazy=True)
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(50), nullable = False)
+    model = db.Column(db.String(50), nullable = False)
+    year = db.Column(db.Integer(4), nullable = False)
+    drivers_id = db.Column(db.Integer, db.ForeignKey('drivers.id'), nullable = False)
+
+
