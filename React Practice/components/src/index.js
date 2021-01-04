46ff6e720e60a0dfe9d6fb85dd0bcf1d4c616971
Brandon <bredmond1019@gmail.com>
2021-01-04 08:21:47 -0500

    React Approval Card

--- React Practice/components/src/index.js
+++ React Practice/components/src/index.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
-import faker from 'faker';
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam"  timeAgo="Today at 4:45PM" profilePic={faker.image.image()} comment="Awesome!"/>
-            <CommentDetail author="Jane" timeAgo="Today at 2:00AM" profilePic={faker.image.image()} comment="Thanks, dude. Appreciate you!"/>
-            <CommentDetail author="Alex" timeAgo="Yesterday at 1:45PM" profilePic={faker.image.image()} comment="Hahahaha"/>
-        </div>
-    )
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          profilePic={faker.image.image()}
+          comment="Awesome!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 2:00AM"
+          profilePic={faker.image.image()}
+          comment="Thanks, dude. Appreciate you!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Yesterday at 1:45PM"
+          profilePic={faker.image.image()}
+          comment="Hahahaha"
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
-
-
+ReactDOM.render(<App />, document.querySelector("#root"));
