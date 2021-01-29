95bc15e90f8a343715dcc9cc31ef05b5c37d9332
Brandon <bredmond1019@gmail.com>
2021-01-29 11:15:53 -0500

    React Router

--- React Practice/navegacao/src/views/App.jsx
+++ React Practice/navegacao/src/views/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Content from "../components/layout/Content";
 import Menu from "../components/layout/Menu";
 import "./App.css";
 
 const App = (props) => (
   <div className="App">
-    <Menu />
-    <Content />
+    <Router>
+      <Menu />
+      <Content />
+    </Router>
   </div>
 );
 
