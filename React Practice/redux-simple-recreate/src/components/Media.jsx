0476af8b7751cf448a79852c6eabb9b721e2c60e
bredmond1019 <bredmond1019@penguin>
2021-02-19 10:09:23 -0500

    Replicated Redux App

---
+++ React Practice/redux-simple-recreate/src/components/Media.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+
+import { connect } from "react-redux";
+
+const Media = (props) => {
+  const { min, max } = props;
+  return (
+    <Card title="Media dos numeros" green>
+      <span>
+        Resultado: <strong>{(min + max) / 2}</strong>
+      </span>
+    </Card>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+export default connect(mapStateToProps)(Media);
