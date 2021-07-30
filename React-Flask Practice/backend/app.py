81988292507328bb70bfdd74c2635d77f8b724ed
bredmond1019 <bredmond1019@penguin>
2021-07-30 12:55:50 -0400

    Added GET & ADD routes

--- React-Flask Practice/backend/app.py
+++ React-Flask Practice/backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_marshmallow import Marshmallow #Serializes the Data
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bredmond1019:flask@localho
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app) 
 
 
 class Articles(db.Model):
@@ -21,9 +23,41 @@ class Articles(db.Model):
         self.title = title
         self.body =body
 
-@app.route('/', methods = ['GET'])
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', "title", "body", 'date')
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many = True)
+
+
+@app.route('/get', methods = ['GET'])
 def get_articles():
-    return jsonify({"Hello": "World"})
+    all_articles = Articles.query.all()
+    results = articles_schema.dump(all_articles)
+    return jsonify(results)
+
+@app.route('/get/<id>/', methods = ['GET'])
+def post_details(id):
+    article = Articles.query.get(id)
+    return article_schema.jsonify(article)
+
+
+@app.route('/add', methods = ['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+
+    article = Articles(title, body)
+    db.session.add(article)
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
