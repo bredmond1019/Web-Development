81405e85ed73ae24aadcee0106185614ebeb3c89
bredmond1019 <bredmond1019@gmail.com>
2022-04-13 20:51:05 -0400

    Updated TakeHome

--- HealthierTakeHome/ModelDesignProblem/backend/graphql/objects.py
+++ HealthierTakeHome/ModelDesignProblem/backend/graphql/objects.py
@@ -2,11 +2,46 @@ import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
-from ..models import Wombat as WombatModel
+
+from ..models import \
+    Provider as ProviderModel, \
+    Client as ClientModel, \
+    JournalEntry as JournalModel
 
 
-class WombatObject(SQLAlchemyObjectType):
+class ProviderObject(SQLAlchemyObjectType):
 
     class Meta:
-        model = WombatModel
+        model = ProviderModel
         interfaces = (relay.Node, )
+
+
+class ClientObject(SQLAlchemyObjectType):
+
+    class Meta:
+        model = ClientModel
+        interfaces = (relay.Node, )
+
+    journal_entries = graphene.List(
+        lambda: JournalObject, 
+        entry=graphene.String(), date_entered=graphene.DateTime() 
+        )
+
+    def resolve_journal_entries(self, info):
+        query = JournalObject.get_query(info=info)
+        query = query.filter(JournalModel.user_id == self.id)
+        query.order_by(JournalModel.date_entered)
+
+        return query.all()
+
+
+
+class JournalObject(SQLAlchemyObjectType):
+
+     class Meta:
+        model = JournalModel
+        interfaces = (relay.Node, )
+
+
+class JournalInput(graphene.InputObjectType):
+    entry = graphene.String()
--- HealthierTakeHome/ModelDesignProblem/backend/models.py
+++ HealthierTakeHome/ModelDesignProblem/backend/models.py
@@ -1,23 +1,69 @@
+from enum import unique
 from backend import db
+from datetime import datetime
 
 
-class Wombat(db.Model):
-    __tablename__ = 'wombats'
+client_provider = db.Table('client_provider',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('provider_id', db.Integer, db.ForeignKey('provider.id')),
+    db.Column('plan', db.Enum("basic", "premium", name="provider_plan"))
+    )
+
+
+class Provider(db.Model):
+    __tablename__ = 'provider'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    dob = db.Column(db.Date)
+    email = db.Column(db.String(50), unique=True)
 
-    def __init__(self, name, dob):
+    clients = db.relationship("Client", secondary=client_provider, backref='providers')
+
+    def __init__(self, name, email):
         self.name = name
-        self.dob = dob
+        self.email = email
 
     def __repr__(self):
-        return f"<Wombat {self.name}, born: {self.dob}>"
+        return f"<Provider {self.name}, email: {self.email}>"
 
     def to_dict(self):
         data = {
             'id': self.id,
             'name': self.name,
-            'dob': self.dob.isoformat()
+            'email': self.email
         }
         return data
+
+
+
+
+class Client(db.Model):
+    __tablename__ = 'client'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+
+    journal_entries = db.relationship("JournalEntry", backref="client")
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f"<Provider {self.name}, email: {self.email}>"
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email, 
+            "journal_entries" : self.journal_entries
+        }
+        return data
+
+
+class JournalEntry(db.Model):
+    __tablename__ = 'journal_entry'
+    id = db.Column(db.Integer, primary_key=True)
+    entry = db.Column(db.String(5000))
+    user_id = db.Column(db.Integer, db.ForeignKey("client.id"))
+    date_entered = db.Column(db.DateTime(), default=datetime.now())
